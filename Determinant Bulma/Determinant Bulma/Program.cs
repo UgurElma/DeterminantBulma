7499f06d71281cb6df60c25caf87bfa65fe693db
UgurElma <154337639+UgurElma@users.noreply.github.com>
2024-06-01 15:48:54 +0300

    DeterminantBulma
    Girilen nxn boyutlu bir matrisin determinantını, kofaktör yöntemi ile bulmak ve kullanıcıya göstermek.

--- Determinant Bulma/Determinant Bulma/Program.cs
+++ Determinant Bulma/Determinant Bulma/Program.cs
@@ -1,82 +1,134 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Reflection;
-namespace DeterminantBulma
+internal class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main()
+        Console.Write("nxn boyutundaki bir kare matrisinin determinantını, kofaktör yöntemiyle bulan uygulamaya hoş geldiniz.\nDevam etmek için klavyeden herhangi bir tuşa basınız!");
+        Console.ReadKey();
+        while (true)
         {
-            Console.WriteLine("3x3 boyutundaki bir A matrisinin determinantını, kofaktör yönetemiyle bulma programına hoş geldiniz.\nDevam etmek için bir tuşa basınız!");
-            Console.ReadKey();
-            while (true)
+            Console.Clear();
+            int n = 0;
+            while (n < 2)
             {
-            Don:
-                double determinant = 0;
+                string hatan = string.Empty;
                 Console.Clear();
-                Console.WriteLine("Lütfen A matrisinizi giriniz.");
-                double[,] matris = new double[3, 3];
-                for (int i = 1; i <= 3; i++)
+                Console.Write("Lütfen, determinantını bulmak istediğiniz kare matrisin n değerini (en az 2 olmalı) giriniz: ");
+                try
                 {
-                    for (int j = 1; j <= 3; j++)
-                    {
-                        Console.Write("A{0}{1} = ", i, j);
-                        try
-                        {
-                            matris[i - 1, j - 1] = double.Parse(Console.ReadLine());
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine("Hatanız : {0} Tekrardan matrisi girmek için herhangi bir tuşa basınız!", e.Message);
-                            Console.ReadKey();
-                            goto Don;
-                        }
-                    }
+                    n = int.Parse(Console.ReadLine());
+                    if (n < 2)
+                        hatan = "n değeri en az 2 olmalıdır!";
                 }
-                Console.WriteLine("----------------------------");
-                for (int i = 1; i <= 3; i++)
+                catch (Exception hata)
                 {
-                    for (int j = 1; j <= 3; j++)
-                    {
-                        Console.Write(matris[i - 1, j - 1] + " ");
-                    }
-                    Console.WriteLine("");
+                    hatan = hata.Message;
                 }
-                Console.WriteLine("----------------------------\nMatrisiniz doğru mu?\nDoğruysa ---> 2 dışında herhangi bir tuş Yanlış ise ---> 2\n----------------------------");
-                if (Console.ReadLine() == "2")
-                    goto Don;
-                List<double> list = new List<double>();
-                for (int i = 1; i <= 3; i++)
+                if (hatan != string.Empty)
                 {
-                    for (int j = 1; j <= 3; j++)
-                    {
-                        if (i == 1)
-                        {
-                            double katsayi = matris[i - 1, j - 1];
-                            for (int k = 1; k <= 3; k++)
-                            {
-                                for (int m = 1; m <= 3; m++)
-                                {
-                                    if (k != i && m != j)
-                                        list.Add(matris[k - 1, m - 1]);
-                                }
-                            }
-                            double Cij = (double)(Math.Pow(-1, i + j)) * katsayi * ((list[0] * list[3]) - (list[1] * list[2]));
-                            determinant += Cij;
-                            list.Clear();
-                        }
-                    }
+                    Console.Write(hatan + " Devam etmek için klavyeden herhangi bir tuşa basınız!");
+                    Console.ReadKey();
                 }
-                Console.WriteLine("A matrisinizin determinantı, det(A) = {0}", determinant);
-                Console.WriteLine("\n\nUygulamadan çıkmak için klavyeden 'E || e' tuşuna basınız! Devam etmek için 'E || e' dışında herhangi bir tuşa basınız!");
-                char c = Console.ReadKey().KeyChar;
-                if (c == 'E' || c == 'e')
-                    break;
+            }
+        geriDon:
+            double[,] firstMatris = new double[n, n];
+            Console.Clear();
+            Console.WriteLine("Lütfen, determinantını bulmak istediğiniz matrisin indislerini giriniz.");
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write("A{0}{1} = ", i, j);
+                    try
+                    {
+                        firstMatris[i - 1, j - 1] = double.Parse(Console.ReadLine());
+                    }
+                    catch
+                    { Console.Write("Hatalı giriş yaptınız! Tekrardan indisleri girmek için herhangi bir tuşa basınız."); Console.ReadKey(); goto geriDon; }
+                }
             }
             Console.Clear();
-            Console.WriteLine("Ugulamadan çıkış yaptınız!");
-            Console.ReadKey();
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write(firstMatris[i - 1, j - 1] + " ");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("-------------------------------------------------\nMatrisiniz doğru mu? (Doğruysa ---> 1 Yanlışsa ---> 2 Çıkış ---> 'Diğer Tuşlar')");
+            string answer = Console.ReadLine();
+            if (answer == "1")
+            {
+                Console.WriteLine("{0}x{0} boyutundaki kare matrisinizin determinantı = {1}", n, matrisConvertor(firstMatris, n));
+                Console.Write("\nDevam etmek için klavyeden herhangi bir tuşa basınız!");
+                Console.ReadKey();
+            }
+            else if (answer == "2")
+            {
+                goto geriDon;
+            }
+            else
+            {
+                Console.Clear();
+                Console.Write("Uygulama kapatıldı!");
+                Console.ReadKey();
+                break;
+            }
         }
+    }
+    static double matrisConvertor(double[,] firstMatris, int boyut)
+    {
+        double determinant = 0;
+        if (boyut > 2)
+        {
+            for (int i = 1; i <= boyut; i++)
+            {
+                double katsayi = firstMatris[0, i - 1] * Math.Pow(-1, 1 + i);
+                double[,] secondMatris = new double[boyut - 1, boyut - 1];
+                List<double> list = new List<double>();
+                for (int k = 1; k <= boyut; k++)
+                {
+                    for (int l = 1; l <= boyut; l++)
+                    {
+                        if (k != 1 && l != i)
+                        {
+                            list.Add(firstMatris[k - 1, l - 1]);
+                        }
+                    }
+                }
+                int sayac = 0;
+                for (int m = 1; m <= boyut - 1; m++)
+                {
+                    for (int n = 1; n <= boyut - 1; n++)
+                    {
+                        secondMatris[m - 1, n - 1] = list[sayac];
+                        sayac++;
+                    }
+                }
+                if (secondMatris.GetLength(0) > 2)
+                {
+                    determinant += katsayi * matrisConvertor(secondMatris, secondMatris.GetLength(0));
+                }
+                else
+                {
+                    determinant += katsayi * ikiBoyutu(secondMatris);
+                }
+            }
+            return determinant;
+        }
+        else
+        {
+            return ikiBoyutu(firstMatris);
+        }
+    }
+    static double matrisReturn(double[,] inMatris, double katsayi)
+    {
+        return katsayi * (inMatris[0, 0] * inMatris[1, 1] - inMatris[0, 1] * inMatris[1, 0]);
+    }
+    static double ikiBoyutu(double[,] inMatris)
+    {
+        return (inMatris[0, 0] * inMatris[1, 1] - inMatris[0, 1] * inMatris[1, 0]);
     }
 }
